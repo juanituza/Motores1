6018cdc9eceea298b5b8f064299fd2a508f94e8a
kevinrossoln100 <kevinrossodev@gmail.com>
2026-04-24 22:16:00 -0300

    Menu de inicio + git ignore

--- Juego Motores1 El Nene/Assets/Scripts/EnemyMovement/IAEnemigo.cs
+++ Juego Motores1 El Nene/Assets/Scripts/EnemyMovement/IAEnemigo.cs
@@ -5,72 +5,93 @@ using System.Collections;
 
 public class IAEnemigo : MonoBehaviour
 {
-    private NavMeshAgent agent;
+    private NavMeshAgent _agent;
 
     [SerializeField] private Transform player;
-    [SerializeField] private CamaraNino efectoCamara;
+    private PlayerStats _playerStats;
 
-    [SerializeField] private List<Transform> puntosPatrulla;
-    [SerializeField] private float tiempoEsperaPatrulla = 2f;
-    private int indiceActual = 0;
-    private bool esperando = false;
-    private bool atacando = false;
+    [Header("Patrol Settings")]
+    [SerializeField] private List<Transform> wayPoints;
+    [SerializeField] private float patrolWaitTime = 2f;
+    private int _currentWayPointIndex = 0;
+    private bool _isWaiting = false;
+    private bool _isAttacking = false;
 
-    [SerializeField] private float velocidadNormal = 2.5f;
+    [SerializeField] private float normalSpeed = 2.5f;
+
+    [Header("Damage Settings")]
+    [SerializeField] private float _damageCooldown = 2f;
+    private float _lastDamageTime;
 
     private void Awake()
     {
-        agent = GetComponent<NavMeshAgent>();
-        agent.updateRotation = false;
+        _agent = GetComponent<NavMeshAgent>();
+        _agent.updateRotation = false;
+
+        if (player != null) _playerStats = player.GetComponent<PlayerStats>();
     }
 
     private void Start()
     {
-        if (puntosPatrulla.Count > 0) ActualizarDestinoPatrulla();
+        if (wayPoints.Count > 0) UpdatePatrolDestination();
     }
 
     private void Update()
     {
-        if (atacando) return;
+        if (_isAttacking) return;
+        if (!_isWaiting) Patrol();
+
+        ApplyManualRotation();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
         {
-            if (!esperando) Patrullar();
+            if (Time.time > _lastDamageTime + _damageCooldown)
+            {
+                if (_playerStats != null)
+                {
+                    _playerStats.TakeDamage();
+                    _lastDamageTime = Time.time;
+                    Debug.Log("Player hit by enemy!");
+                }
+            }
         }
-
-        AplicarGiroManual();
     }
 
-    private void AplicarGiroManual()
+    private void ApplyManualRotation()
     {
-        Vector3 direccionGiro = agent.velocity;
-        if (atacando) direccionGiro = (player.position - transform.position);
+        Vector3 rotationDirection = _agent.velocity;
+        if (_isAttacking) rotationDirection = (player.position - transform.position);
 
-        if (direccionGiro.sqrMagnitude > 0.1f)
+        if (rotationDirection.sqrMagnitude > 0.1f)
         {
-            Quaternion rotacionObjetivo = Quaternion.LookRotation(new Vector3(direccionGiro.x, 0, direccionGiro.z));
-            transform.rotation = Quaternion.Slerp(transform.rotation, rotacionObjetivo, Time.deltaTime * 10f);
+            Quaternion targetRotation = Quaternion.LookRotation(new Vector3(rotationDirection.x, 0, rotationDirection.z));
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 10f);
         }
     }
 
-    void Patrullar()
+    private void Patrol()
     {
-        agent.isStopped = false;
-        agent.speed = velocidadNormal;
-        if (!agent.pathPending && agent.remainingDistance < 0.5f) StartCoroutine(PausaEnPunto());
+        _agent.isStopped = false;
+        _agent.speed = normalSpeed;
+        if (!_agent.pathPending && _agent.remainingDistance < 0.5f) StartCoroutine(WaitAtPoint());
     }
 
-    IEnumerator PausaEnPunto()
+    private IEnumerator WaitAtPoint()
     {
-        esperando = true;
-        agent.isStopped = true;
-        yield return new WaitForSeconds(tiempoEsperaPatrulla);
-        indiceActual = (indiceActual + 1) % puntosPatrulla.Count;
-        ActualizarDestinoPatrulla();
-        agent.isStopped = false;
-        esperando = false;
+        _isWaiting = true;
+        _agent.isStopped = true;
+        yield return new WaitForSeconds(patrolWaitTime);
+        _currentWayPointIndex = (_currentWayPointIndex + 1) % wayPoints.Count;
+        UpdatePatrolDestination();
+        _agent.isStopped = false;
+        _isWaiting = false;
     }
 
-    void ActualizarDestinoPatrulla()
+    private void UpdatePatrolDestination()
     {
-        agent.SetDestination(puntosPatrulla[indiceActual].position);
+        _agent.SetDestination(wayPoints[_currentWayPointIndex].position);
     }
 }
---
+++ Juego Motores1 El Nene/Assets/Scripts/PlayerMovementsJuan/Movimiento.cs
@@ -0,0 +1,121 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movimiento : MonoBehaviour
+{
+    private CharacterController _controller;
+    private Vector2 _moveInput;
+    private Vector3 _direction;
+
+    public float walkSpeed = 100f;
+    public float runSpeed = 150f;
+    public float gravity = -70f;
+    public float jumpForce = 10f;
+    private float _verticalVelocity;
+    private bool _isRunning;
+
+    [SerializeField] private AudioSource _footstepAudioSource;
+    [SerializeField] private AudioClip _footstepSound;
+    [SerializeField] private float _walkStepInterval = 0.5f;
+    [SerializeField] private float _runStepInterval = 0.3f;
+    private float _stepTimer = 0f;
+
+    [SerializeField] private float _interactRange = 5f;
+    [SerializeField] private LayerMask _interactableLayer;
+
+    private void Awake()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        _moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnRun(InputAction.CallbackContext context)
+    {
+        if (context.performed) _isRunning = true;
+        if (context.canceled) _isRunning = false;
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed && _controller.isGrounded)
+        {
+            _verticalVelocity = Mathf.Sqrt(jumpForce * -2f * gravity);
+        }
+    }
+    void Update()
+    {
+        float currentSpeed = _isRunning ? runSpeed : walkSpeed;
+        _direction = transform.right * _moveInput.x + transform.forward * _moveInput.y;
+        _controller.Move(_direction * currentSpeed * Time.deltaTime);
+
+        if (_controller.isGrounded)
+        {
+            if (_verticalVelocity < 0)
+            {
+                _verticalVelocity = -50f;
+            }
+        }
+        else
+        {
+            _verticalVelocity += gravity * Time.deltaTime;
+        }
+
+        Vector3 fallMovement = new Vector3(0, _verticalVelocity, 0);
+        _controller.Move(fallMovement * Time.deltaTime);
+
+        HandleFootsteps();
+    }
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+            Debug.DrawRay(ray.origin, ray.direction * _interactRange, Color.red, 2f);
+
+            if (Physics.Raycast(ray, out RaycastHit hit, _interactRange, _interactableLayer))
+            {
+                Debug.Log("Hit: " + hit.collider.name);
+
+                IInteractable interactable = hit.collider.GetComponent<IInteractable>();
+                if (interactable != null)
+                {
+                    interactable.Interact();
+                }
+            }
+        }
+    }
+    private void HandleFootsteps()
+    {
+        bool isMoving = _controller.isGrounded && _moveInput.magnitude > 0.1f;
+
+        if (isMoving)
+        {
+            _stepTimer += Time.deltaTime;
+            float currentInterval = _isRunning ? _runStepInterval : _walkStepInterval;
+
+            if (_stepTimer >= currentInterval)
+            {
+                PlayFootstep();
+                _stepTimer = 0f;
+            }
+        }
+        else
+        {
+            _stepTimer = _walkStepInterval;
+        }
+    }
+
+    private void PlayFootstep()
+    {
+        if (_footstepSound != null && _footstepAudioSource != null)
+        {
+            _footstepAudioSource.pitch = Random.Range(0.85f, 1.15f);
+            _footstepAudioSource.PlayOneShot(_footstepSound);
+        }
+    }
+}
+
